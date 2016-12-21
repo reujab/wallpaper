265f7f3da2c7803f8df32c9fb387d5680084d93e
reujab <reujab@gmail.com>
2016-12-20 20:38:09 -0500

    implemented Get() on macOS

--- darwin.go
+++ darwin.go
@@ -6,6 +6,23 @@ import "os/exec"
 import "os/user"
 import "path/filepath"
 import "strconv"
+import "strings"
+
+// Get gets the current wallpaper.
+func Get() (wallpaper string, err error) {
+  output, err := exec.
+    Command("osascript", "-e", `tell application "Finder" to get POSIX path of (get desktop picture as alias)`).
+    Output()
+
+  if err != nil {
+    return
+  }
+
+  // is calling strings.TrimSpace() necessary?
+  wallpaper = strings.TrimSpace(string(output))
+
+  return
+}
 
 // SetFromFile uses AppleScript to tell Finder to set the desktop wallpaper to specified file.
 func SetFromFile(file string) error {
